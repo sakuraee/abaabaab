61cb322bf13d5bfdff7232f6be69767573817a41
sakuraee <56988719+sakuraee@users.noreply.github.com>
2024-12-15 13:32:13 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = (req, res) => {
-    res.status(200).json({ message: 'Hello from Vercel!' });
-  };
+const Koa = require('koa');
+const app = new Koa();
+
+app.use(async ctx => {
+    ctx.body = 'Hello Vercel';
+});
+
+app.listen(3008, () => {
+    console.log('3008项目启动')
+});
